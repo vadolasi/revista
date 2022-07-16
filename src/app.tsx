4076168b0adbc8d8080524bca7e915fdab1ee034
Vitor Daniel <vitor036daniel@gmail.com>
2022-07-16 14:38:40 -0300

    fix layout

--- src/app.tsx
+++ src/app.tsx
@@ -3,14 +3,16 @@ import { html } from "htm/preact"
 
 export function App() {
   return html`
-    <${TransformWrapper} minScale=${.5} centerOnInit=${true}>
-      <${TransformComponent} wrapperClass="w-screen h-screen max-h-screen">
-        <div class="p-30">
-          <div class="w-200 h-300 bg-blue">
-            <p>Lorem ipsum at dolor amet</p>
+    <div class="max-w-screen max-h-screen overflow-hidden">
+      <${TransformWrapper} minScale=${.5} centerOnInit=${true}>
+        <${TransformComponent} wrapperClass="w-screen h-screen bg-red">
+          <div class="p-30">
+            <div class="w-200 h-300 bg-blue">
+              <p>Lorem ipsum at dolor amet</p>
+            </div>
           </div>
-        </div>
+        </>
       </>
-    </>
+    </div>
   `
 }
