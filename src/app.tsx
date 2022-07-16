9b4b61b746e2a33e908d14baa5bf3b818c730c35
Vitor Daniel <vitor036daniel@gmail.com>
2022-07-16 14:51:15 -0300

    fix layout

--- src/app.tsx
+++ src/app.tsx
@@ -4,7 +4,7 @@ import { html } from "htm/preact"
 export function App() {
   return html`
     <${TransformWrapper} minScale=${.5} centerOnInit=${true}>
-      <${TransformComponent} wrapperClass="bg-red" wapperStyle=${{ width: "100vw", height: "100vh" }}>
+      <${TransformComponent} wrapperClass="bg-red" wrapperStyle=${{ width: "100vh", height: "100vw" }}>
         <div class="p-30">
           <div class="w-200 h-300 bg-blue">
             <p>Lorem ipsum at dolor amet</p>
