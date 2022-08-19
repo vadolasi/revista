c00451ef216e9839d2db11272e7d9e78638d305e
Vitor Daniel <vitor036daniel@gmail.com>
2022-08-18 21:30:24 -0300

    add pages

--- src/components/Bottom.tsx
+++ src/components/Bottom.tsx
@@ -4,20 +4,22 @@ import { FunctionComponent } from "preact"
 interface Props {
   pagesNumber: number
   currentPage: number
+  onNextPage: () => void
+  onPreviousPage: () => void
 }
 
-export const Bottom: FunctionComponent<Props> = ({ currentPage, pagesNumber }) => {
+export const Bottom: FunctionComponent<Props> = ({ currentPage, pagesNumber, onNextPage, onPreviousPage }) => {
   const pagesProgress = currentPage * (100 / pagesNumber)
 
-  return html`
+  return (
     <div class="w-full bg-blue h-15 flex flex-col">
-      <div class="h-1 bg-yellow" style="width: ${pagesProgress}%"></div>
+      <div class="h-1 bg-yellow" style={{ width: `${pagesProgress}%` }}></div>
       <div class="flex items-center content-center justify-center">
         <div class="flex">
-          <button>←</button>
-          <button>→</button>
+          <button class="bg-transparent text-gray-200 border-0 text-3xl" onClick={() => onPreviousPage()}><i class="fa-solid fa-arrow-left"></i></button>
+          <button class="bg-transparent text-gray-200 border-0 text-3xl" onClick={() => onNextPage()}><i class="fa-solid fa-arrow-right"></i></button>
         </div>
       </div>
     </div>
-  `
+  )
 }
--- src/pages/Magazine.tsx
+++ src/pages/Magazine.tsx
@@ -1,18 +1,37 @@
 import { TransformWrapper, TransformComponent } from "@pronestor/react-zoom-pan-pinch"
-import { html } from "htm/preact"
 import { FunctionComponent } from "preact"
+import { useEffect, useState } from "preact/hooks"
 import { Bottom } from "../components/Bottom"
-import Capa from "./Capa.svg"
 
 export const Maganize: FunctionComponent = () => {
-  return html`
-    <div class="w-screen h-screen flex flex-col">
-      <${TransformWrapper} minScale=${.25} initialScale=${.5} centerOnInit=${true}>
-        <${TransformComponent} wrapperClass="w-full">
-          <img src=${Capa} />
-        </>
-      </>
-      <${Bottom} pagesNumber=${7} currentPage=${1} />
+  const [currentPage, setCurrentPage] = useState(1)
+  const [currentImage, setCurrentImage] = useState<string>()
+
+  useEffect(() => {
+    setCurrentImage(new URL(`./magazine/${currentPage}.svg`, import.meta.url).toString())
+  } , [currentPage])
+
+  return (
+    <div class="!w-screen !h-screen flex flex-col">
+      <TransformWrapper minScale={.25} initialScale={.5} centerOnInit={true}>
+        <TransformComponent wrapperClass="!w-full">
+          <img src={currentImage} />
+        </TransformComponent>
+      </TransformWrapper>
+      <Bottom
+        pagesNumber={9}
+        currentPage={currentPage}
+        onPreviousPage={() => {
+          if (currentPage > 1) {
+            setCurrentPage(currentPage - 1)
+          }
+        }}
+        onNextPage={() => {
+          if (currentPage < 9) {
+            setCurrentPage(currentPage + 1)
+          }
+        }}
+      />
     </div>
-  `
+  )
 }
