89d8ecab1c61163d5eac5396b9f7b2d2e36b40a5
Vitor Daniel <vitor036daniel@gmail.com>
2022-08-22 20:37:34 -0300

    add dynamic import

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite"
 import preact from "@preact/preset-vite"
 import Unocss from "unocss/vite"
+import dynamicImport from "vite-plugin-dynamic-import"
 
 export default defineConfig({
   plugins: [
@@ -14,7 +15,8 @@ export default defineConfig({
         ]
       }
     }),
-    Unocss()
+    Unocss(),
+    dynamicImport()
   ],
   server: {
     host: true
