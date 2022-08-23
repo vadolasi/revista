c8dec30265a6fce23a84dcc04c099a456c9e5ff1
Vitor Daniel <vitor036daniel@gmail.com>
2022-08-22 21:59:24 -0300

    fix

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,7 @@
 import { FunctionComponent } from "preact"
 import { useGoogleLogin } from "react-google-login"
 import { gapi } from "gapi-script"
-import { useEffect } from "preact/hooks"
+import { useEffect, useState } from "preact/hooks"
 
 const clientId = "590579873075-uhs8opgdkc82dh2sttob6aq649bbalak.apps.googleusercontent.com"
 
@@ -27,8 +27,11 @@ export const LoginPage: FunctionComponent = () => {
     })
   })
 
+  const [test, setTest] = useState("a")
+
   return (
     <div>
+      <button onClick={() => setTest(test => test + "a")}>{test}</button>
       <button onClick={signIn}>Entrar com o google</button>
     </div>
   )
